9f1f64daa53fc870bf12792c274cdf475eb5f894
sasta-kro <sai.aike.shwe.tun.aung@gmail.com>
2026-03-01 17:45:01 +0700

    built a react template with vite

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+          {/*<h1>Test Website For Corvus Github Deployment</h1>*/}
+          {/*<p>*/}
+          {/*  This is a test website for Corvus Github Deployment.<br/>*/}
+          {/*    It is not intended to be used as a real website.<br/>*/}
+          {/*    It is only intended to be used for testing purposes.<br/>*/}
+          {/*</p>*/}
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
